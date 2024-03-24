e4c6600900f561b8b8cfa8f4d18e370bb8807301
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-23 20:32:53 -0400

    add: env file support

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
 )
+
+func getEnvVar(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
 
 func ping(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "Hello!"})
@@ -14,5 +26,5 @@ func main() {
 	router := gin.Default()
 	router.GET("/", ping)
 
-	router.Run("localhost:8080")
+	router.Run("localhost:" + getEnvVar("PORT"))
 }

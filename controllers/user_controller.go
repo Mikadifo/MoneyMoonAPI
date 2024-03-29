16a2ef3df7a045ad49a64c67318cd2411b0a4446
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-29 13:43:02 -0400

    add: unique user validation

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -31,7 +32,18 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	newUser := models.User{
+	newUser, err := getUserByEmail(user.Email)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+		return
+	}
+
+	if newUser.Email == user.Email {
+		c.IndentedJSON(http.StatusConflict, responses.UserResponse{Status: http.StatusConflict, Message: "error", Data: map[string]interface{}{"data": "User already exists with the following email: " + user.Email}})
+		return
+	}
+
+	newUser = models.User{
 		Username: user.Username,
 		Email:    user.Email,
 		Password: user.Password,
@@ -45,4 +57,21 @@ func CreateUser(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusCreated, responses.UserResponse{Status: http.StatusCreated, Message: "success", Data: map[string]interface{}{"data": result}})
+}
+
+func getUserByEmail(email string) (models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var user models.User
+	defer cancel()
+
+	err := userCollection.FindOne(ctx, bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return models.User{}, nil
+		}
+
+		return models.User{}, err
+	}
+
+	return user, nil
 }

effc612d21a4ff2e49b406db7ac9d4461e6a52a3
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-30 11:36:23 -0400

    mod: default response moved to a reusable file

--- responses/default_response.go
+++ responses/default_response.go
@@ -1,7 +1,31 @@
 package responses
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Message int
+
+const (
+	SUCCESS Message = iota
+	ERROR
+)
+
 type DefaultResponse struct {
-	Status  int                    `json:"status"`
-	Message string                 `json:"message"`
-	Data    map[string]interface{} `json:"data"`
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func (message Message) String() string {
+	return [...]string{"success", "error"}[message]
+}
+
+func Send(c *gin.Context, status int, message Message, data interface{}) {
+	response := DefaultResponse{
+		Status:  status,
+		Message: message.String(),
+		Data:    data,
+	}
+	c.IndentedJSON(status, response)
 }

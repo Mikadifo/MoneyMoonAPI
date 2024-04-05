b1cfd0ebd30b088df3532c1d70ba1c56dc9ba2cf
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-05 17:03:06 -0400

    add: jwt validation on transaction get

--- src/routes/transaction_route.go
+++ src/routes/transaction_route.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"mikadifo/money-moon/src/controllers"
+	"mikadifo/money-moon/src/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func TransactionRoute(router *gin.Engine) {
-	router.GET("/transactions/:bankId", controllers.GetAllTransactionsByBankId)
 	router.POST("/transactions/create", controllers.CreateTransaction)
+	router.GET("/transactions/:bankId", middleware.RequireAuth, controllers.GetAllTransactionsByBankId)
 }

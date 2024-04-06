4797b8b6a1ada17b4e512625f0caf0ed15b86bf7
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-05 21:54:26 -0400

    fix: transaction model bson

--- src/models/transaction.go
+++ src/models/transaction.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Transaction struct {
 	Id          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	Date        string             `json:"date,omitempty" validate:"required"`
-	BankId      string             `json:"bankId,omitempty" validate:"required"`
+	BankId      string             `bson:"bankId,omitempty" validate:"required" json:"bankId,omitempty"`
 	Type        string             `json:"type,omitempty" validate:"required"`
 	Description string             `json:"description,omitempty" validate:"required"`
 	Amount      float64            `json:"amount,omitempty" validate:"required"`
